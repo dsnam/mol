869005d01d9c5c853ce1fae3969f41e6a113f5a4
dsnam <dodsonn@gmail.com>
2020-02-05 23:39:27 -0500

    remove nom, start on lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,8 @@
-extern crate nom;
-
-use nom::*;
+use std::borrow::{Borrow, BorrowMut};
+use std::io::{self, Write};
+use std::iter::Peekable;
+use std::ops::{Deref, DerefMut};
+use std::str::Chars;
 use Token::*;
 
 #[derive(Debug, PartialEq)]
@@ -13,7 +15,137 @@ pub enum Token {
     RightSqBracket,
     Comma,
     Identifier(String),
+    Int(i32),
+    BoolFalse,
+    BoolTrue,
+    Comment,
+    Op(char),
 }
 
-named!(pub lex_fn<&str, Token>, map!(tag!("fn"), |_| FnDef));
-//named!(pub lex_identifier<&str, Token>, map!(alpha1, |s| Identifier(s.to_owned())));
+#[derive(Debug)]
+pub struct LexerError {
+    pub error: String,
+}
+
+impl LexerError {
+    pub fn new(error_msg: String) -> Self {
+        Self { error: error_msg }
+    }
+}
+
+#[derive(Debug)]
+pub struct TokenInfo {
+    pub token: Token,
+    pub line: i32,
+    pub row: i32,
+}
+
+impl TokenInfo {
+    pub fn new(token: Token, line: i32, row: i32) -> Self {
+        Self { token, line, row }
+    }
+}
+
+pub type LexerResult = Result<TokenInfo, LexerError>;
+
+pub struct Lexer<'a> {
+    input: &'a str,
+    chars: Box<Peekable<Chars<'a>>>,
+    line: i32,
+    row: i32,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Lexer<'a> {
+        Lexer {
+            input,
+            chars: Box::new(input.chars().peekable()),
+            line: 0,
+            row: 0,
+        }
+    }
+
+    fn wrap_token(&mut self, tok: Token) -> LexerResult {
+        return Ok(TokenInfo::new(tok, self.line, self.row));
+    }
+
+    fn wrap_error(&mut self, c: char) -> LexerResult {
+        let mut msg = String::from("Unconsumable token: ");
+        msg.push(c);
+        return Err(LexerError::new(String::from(msg)));
+    }
+
+    pub fn lex(&mut self) -> LexerResult {
+        let chars = self.chars.deref_mut();
+        let src = self.input;
+
+        let mut row = self.row;
+        let mut line = self.line;
+
+        loop {
+            {
+                let ch = chars.peek();
+                if ch.is_none() {
+                    self.line = line;
+                    self.row = row;
+                    return self.wrap_token(EOF);
+                }
+                if ch.unwrap().eq('\n'.borrow()) {
+                    line += 1;
+                    row = -1;
+                }
+                if !ch.unwrap().is_whitespace() {
+                    break;
+                }
+            }
+            chars.next();
+            row += 1;
+        }
+        let start = row;
+        let next = chars.next();
+        if next.is_none() {
+            return self.wrap_token(EOF);
+        }
+
+        let result = match next.unwrap() {
+            '(' => self.wrap_token(LeftParen),
+            ')' => self.wrap_token(RightParen),
+            ',' => self.wrap_token(Comma),
+            '[' => self.wrap_token(LeftSqBracket),
+            ']' => self.wrap_token(RightSqBracket),
+            '/' => {
+                let next = chars.peek();
+                if next == Some(&'/') {
+                    loop {
+                        let c = chars.next();
+                        row += 1;
+                        if c == Some('\n') {
+                            break;
+                        }
+                    }
+                    self.wrap_token(Comment)
+                } else {
+                    self.wrap_token(Op('/'))
+                }
+            }
+            c => self.wrap_error(c),
+        };
+        self.line = line;
+        self.row = row;
+        result
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.lex() {
+            Err(_) => None,
+            Ok(info) => match info.token {
+                EOF => None,
+                tok => Some(tok),
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use lexer::*;
 fn main() {
     loop {
         let mut input = String::new();
-        while !input.contains(";") {
+        while !input.contains("\n") {
             let mut next_line = String::new();
             std::io::stdin().read_line(&mut input).unwrap();
             input.push_str(next_line.as_str())
         }
-        println!("lexing: {:?}", lex_fn(input.as_str()))
+        let mut lexer = Lexer::new(&input);
+        let tokens = lexer.by_ref().collect::<Vec<_>>();
+        println!("lexing: {:?}", tokens)
     }
 }
